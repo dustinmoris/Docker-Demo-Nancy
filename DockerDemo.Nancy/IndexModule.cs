98abd917f61c75f70e031e1fb33d1f7bb63e421c
Dustin Moris Gorski <dustinmoris@gmail.com>
2016-01-04 12:14:12 +0000

    Added project

---
+++ DockerDemo.Nancy/IndexModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Nancy;
+
+namespace DockerDemo.Nancy
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            var secureKey = Environment.GetEnvironmentVariable("SecureKey");
+            var environment = Environment.GetEnvironmentVariable("Environment");
+
+            Get["/"] = _ => "Nancy: Hello World";
+            Get["/os"] = _ => Environment.OSVersion.ToString();
+            Get["/securekey"] = _ => secureKey;
+            Get["/environment"] = _ => environment;
+        }
+    }
+}
---
+++ DockerDemo.Nancy/Program.cs
@@ -0,0 +1,40 @@
+﻿using Mono.Unix;
+using Mono.Unix.Native;
+using Nancy.Hosting.Self;
+using System;
+
+namespace DockerDemo.Nancy
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string uri = "http://localhost:8888";
+
+            Console.WriteLine("Starting Nancy on " + uri);
+
+            var host = new NancyHost(new Uri(uri));
+            host.Start();
+
+            // Check if we're running on mono
+            if (Type.GetType("Mono.Runtime") != null)
+            {
+                // On mono, processes will usually run as daemons - this allows you to listen
+                // for termination signals (CTRL + C, Shutdown, etc.) and finalize correctly
+                UnixSignal.WaitAny(new[] {
+                    new UnixSignal(Signum.SIGINT),
+                    new UnixSignal(Signum.SIGTERM),
+                    new UnixSignal(Signum.SIGQUIT),
+                    new UnixSignal(Signum.SIGHUP)
+                });
+            }
+            else
+            {
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Stopping Nancy");
+            host.Stop();
+        }
+    }
+}
